d2deb74b9075c85a8f74e0edde99575e971550f1
TauaFrade <tau.valentim@gmail.com>
2024-11-01 02:07:31 -0300

    Dominio: Criação do dominio Codigo #2

---
+++ lib/dominios/codigo/codigo.h
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "../../shared/estado_teste.h"
+#include <string>
+
+
+using namespace std;
+
+class Codigo
+{ // Tauã Frade - 231021389
+private:
+    const int TAMANHO = 6;
+    string valor;
+    void validar(string);
+
+public:
+    void setValor(string);
+    string getValor() const;
+};
+
+inline string Codigo::getValor() const
+{
+    return valor;
+}
+
+// Teste
+
+class TUCodigo
+{
+private:
+    const string VALORES_VALIDOS[5] = {"ABCDEF" , "d8eT5e" , "78J980", "123456", "m12E23"};
+    const string VALORES_INVALIDOS[5] = {"-asetf" , "#@%!&*" , "12@4" , "mc@l+teegis" , " "};
+    Codigo *codigo;
+    EstadoTeste estado;
+    void setUp();
+    void tearDown();
+    void testarCenariosSucesso();
+    void testarCenariosFalha();
+
+public:
+    EstadoTeste run();
+};
