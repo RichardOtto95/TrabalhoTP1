123d1a4b40cc86ad5ed06ca0434a7e820b11d56b
michelenakagomi <148566592+michelenakagomi@users.noreply.github.com>
2024-11-01 18:32:18 -0300

    Update dominio.cpp

--- dominio.cpp
+++ dominio.cpp
@@ -1,19 +1,42 @@
 #include "dominio.hpp"
+#include <iostream>
 using namespace std;
 
-bool Nome::validar(string valor){
-    if (valor.size()>30){
+bool Nome::validar(string nome){
+    if (nome.size()>30){
         return false;
     }
     return true;
 
 }
 
-bool Nome::setValor(string valor){
-    if(!validar(valor))
+bool Nome::setNome(string nome){
+    if(!validar(nome))
         return false;
-    this->valor=valor;
+    this->nome=nome;
     return true;
 
 
+}
+
+void TUNome::run() {
+    Nome nome;
+
+    if (nome.setNome("Michele")) {
+        cout << "Teste com valor válido passou." << endl;
+    } else {
+        cout << "Teste com valor válido falhou." << endl;
+    }
+
+    if (!nome.setNome("Michelenakagomilebarbenchonsilvacosta")) {
+        cout << "Teste com valor inválido passou." << endl;
+    } else {
+        cout << "Teste com valor inválido falhou." << endl;
+    }
+}
+
+int main() {
+    TUNome testeNome;
+    testeNome.run();
+    return 0;
 }
